cc061641a6df98daafc2a07c429505610bcbbd58
jonasfeld <29547401+jonasfeld@users.noreply.github.com>
2024-03-25 11:18:08 +0100

    gdm

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,7 @@
   '';
   security.pam.services.swaylock.text = "auth include login";
   security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.login.enableGnomeKeyring = true;
 
   hardware = {
     opengl.enable = true;
@@ -237,4 +238,33 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+
+  nixpkgs.overlays = [
+    (
+      self: super: {
+        gnome = super.gnome.overrideScope' (
+          selfg: superg: {
+            gnome-shell = superg.gnome-shell.overrideAttrs (
+              old: {
+                patches =
+                  (old.patches or [])
+                  ++ [
+                    (pkgs.writeText "bg.patch" ''
+                      --- a/data/theme/gnome-shell-sass/widgets/_login-lock.scss
+                      +++ b/data/theme/gnome-shell-sass/widgets/_login-lock.scss
+                      @@ -15,4 +15,5 @@ $_gdm_dialog_width: 23em;
+                       /* Login Dialog */
+                       .login-dialog {
+                         background-color: $_gdm_bg;
+                      +  background-color: #24273a;
+                       }
+                    '')
+                  ];
+              }
+            );
+          }
+        );
+      }
+    )
+  ];
 }
