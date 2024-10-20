47672f132bb8aecab05bd7ac429c80954790095b
jonasfeld <29547401+jonasfeld@users.noreply.github.com>
2024-10-20 13:46:05 +0200

    who needs gdm styling anyways?

--- configuration.nix
+++ configuration.nix
@@ -235,34 +235,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
-  nixpkgs.overlays = [
-    (
-      self: super: {
-        gnome = super.gnome.overrideScope (
-          selfg: superg: {
-            gnome-shell = superg.gnome-shell.overrideAttrs (
-              old: {
-                patches =
-                  (old.patches or [])
-                  ++ [
-                    (pkgs.writeText "bg.patch" ''
-                      --- a/data/theme/gnome-shell-sass/widgets/_login-lock.scss
-                      +++ b/data/theme/gnome-shell-sass/widgets/_login-lock.scss
-                      @@ -15,4 +15,4 @@ $_gdm_dialog_width: 23em;
-                       /* Login Dialog */
-                       .login-dialog {
-                      -  background-color: $_gdm_bg;
-                      +  background-color: #24273a;
-                       }
-                    '')
-                  ];
-              }
-            );
-          }
-        );
-      }
-    )
-  ];
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [25565];
